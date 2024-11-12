922a06ee24458e3b3888337858f38f96f85b73d5
SeungBum Kim <picxenk@gmail.com>
2024-11-12 15:11:51 +0900

    예전 OOP방식을 새 방식으로 리팩토링

--- Particle.js
+++ Particle.js
@@ -1,65 +1,65 @@
 // A simple Particle class
 
-// class Particle {
-//     constructor() {
+class Particle {
+    constructor(position) {
+        this.acceleration = createVector(0, 0.05);
+        this.velocity = createVector(random(-1, 1), random(-1, 0));
+        this.position = position.copy();
+        this.lifespan = 255;
+    }
 
-//     }
+    run() {
+        this.update();
+        this.display();
+    }
 
-//     run() {
+    update() {
+        this.velocity.add(this.acceleration);
+        this.position.add(this.velocity);
+        this.lifespan -= 2;
+    }
 
-//     }
+    display() {
+        stroke(200, this.lifespan);
+        strokeWeight(2);
+        fill(127, this.lifespan);
+        ellipse(this.position.x, this.position.y, 12, 12);
+    }
 
-//     update() {
-
-//     }
-
-//     display() {
-
-//     }
-
-//     isDead() {
-
-//     }
-// }
-
-
-
-
-
-
+    isDead() {
+        return this.lifespan < 0;
+    }
+}
 
 
 
 
 
-let Particle = function(position) {
-    this.acceleration = createVector(0, 0.05);
-    this.velocity = createVector(random(-1, 1), random(-1, 0));
-    this.position = position.copy();
-    this.lifespan = 255;
-  };
+
+
+
+
+
+
+// let Particle = function(position) {
+
+//   };
   
-  Particle.prototype.run = function() {
-    this.update();
-    this.display();
-  };
+//   Particle.prototype.run = function() {
+
+//   };
   
-  // Method to update position
-  Particle.prototype.update = function(){
-    this.velocity.add(this.acceleration);
-    this.position.add(this.velocity);
-    this.lifespan -= 2;
-  };
+//   // Method to update position
+//   Particle.prototype.update = function(){
+
+//   };
   
-  // Method to display
-  Particle.prototype.display = function() {
-    stroke(200, this.lifespan);
-    strokeWeight(2);
-    fill(127, this.lifespan);
-    ellipse(this.position.x, this.position.y, 12, 12);
-  };
+//   // Method to display
+//   Particle.prototype.display = function() {
+
+//   };
   
-  // Is the particle still useful?
-  Particle.prototype.isDead = function(){
-    return this.lifespan < 0;
-  };
+//   // Is the particle still useful?
+//   Particle.prototype.isDead = function(){
+
+//   };
