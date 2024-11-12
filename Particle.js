83af70eab1ae92680fa61d00178a12a6f18e725d
SeungBum Kim <picxenk@gmail.com>
2024-11-12 15:07:53 +0900

    클래스를 파일로 분리했음

---
+++ Particle.js
@@ -0,0 +1,65 @@
+// A simple Particle class
+
+// class Particle {
+//     constructor() {
+
+//     }
+
+//     run() {
+
+//     }
+
+//     update() {
+
+//     }
+
+//     display() {
+
+//     }
+
+//     isDead() {
+
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+let Particle = function(position) {
+    this.acceleration = createVector(0, 0.05);
+    this.velocity = createVector(random(-1, 1), random(-1, 0));
+    this.position = position.copy();
+    this.lifespan = 255;
+  };
+  
+  Particle.prototype.run = function() {
+    this.update();
+    this.display();
+  };
+  
+  // Method to update position
+  Particle.prototype.update = function(){
+    this.velocity.add(this.acceleration);
+    this.position.add(this.velocity);
+    this.lifespan -= 2;
+  };
+  
+  // Method to display
+  Particle.prototype.display = function() {
+    stroke(200, this.lifespan);
+    strokeWeight(2);
+    fill(127, this.lifespan);
+    ellipse(this.position.x, this.position.y, 12, 12);
+  };
+  
+  // Is the particle still useful?
+  Particle.prototype.isDead = function(){
+    return this.lifespan < 0;
+  };
