c0d3d206663bce61c0c98d5ab1ff474481147ea1
SeungBum Kim <picxenk@gmail.com>
2024-11-12 16:07:12 +0900

    Revert "파티클 크기 속성 리팩토링"
    This reverts commit e53f40f2097b7a06798ee8deb21d4e31a0be31d5.

--- Particle.js
+++ Particle.js
@@ -6,7 +6,6 @@ class Particle {
         this.velocity = createVector(random(-1, 1), random(-1, 0));
         this.position = position.copy();
         this.lifespan = 255;
-        this.w = 2;
     }
 
     run() {
@@ -30,7 +29,7 @@ class Particle {
         stroke(200, this.lifespan);
         strokeWeight(2);
         fill(127, this.lifespan);
-        ellipse(this.position.x, this.position.y, this.w, this.w);
+        ellipse(this.position.x, this.position.y, 12, 12);
     }
 
     isDead() {
